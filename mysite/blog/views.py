c06e0ca598af34e26591dd9bd0b9654b7543cfa3
yosephbernandus <yosephbernandus@gmail.com>
2022-07-11 21:53:58 +0700

    Add flake8 check

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,3 +6,5 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
