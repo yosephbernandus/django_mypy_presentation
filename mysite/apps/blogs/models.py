d8967d354a2a426eddd8c08751534fc6b6df1bcb
yosephbernandus <yosephbernandus@gmail.com>
2022-07-12 06:51:43 +0700

    add mypy

--- mysite/apps/blogs/models.py
+++ mysite/apps/blogs/models.py
@@ -10,9 +10,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self) -> str:
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self) -> int:
+    def __str__(self):
         return self.title
